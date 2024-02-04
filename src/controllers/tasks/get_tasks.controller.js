e522f830310411ea92eaadef2c98dfe40a15fd8b
Cristian Rodriguez <cuentagit1111@gmail.com>
2024-02-04 17:01:49 -0500

    new Commit

--- src/controllers/tasks/get_tasks.controller.js
+++ src/controllers/tasks/get_tasks.controller.js
@@ -1,7 +1,7 @@
 import { Tasks } from "../../db/models/index.models.js";
 
 export async function get_tasks(req, res) {
-  const tasks = await Tasks.findAll();
+  const tasks = await Tasks.findAll({ limit: 10 });
   const jsonResponse = {
     message: "All tasks in my database",
     data: tasks,
